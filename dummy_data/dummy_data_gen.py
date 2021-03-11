8dc9d64e1d5e3f95d4c100c65719e78022c25444
G.efstathiadis <54844705+GeorgeEfstathiadis@users.noreply.github.com>
2021-03-11 08:24:57 +0000

    example data

---
+++ dummy_data/dummy_data_gen.py
@@ -0,0 +1,41 @@
+import os 
+import pandas as pd
+import random
+
+os.chdir('Desktop')
+
+
+
+ids = ['m8PDtlLB4h',
+       'lnxbM8b6z6',
+       'nbFk9o5oLS',
+        '3vyLLz5nZh',
+        'FxbA5YpEHk',
+        'R3RIn8m3DK',
+        'uMoYIqoOYe',
+        'IljqftXu0f',
+        'JMzRy5f52y',
+        'L0DdF6SZOG']
+
+ids = [[i] * 3 for i in ids]
+ids = [i for s in ids for i in s]
+
+pathnos = [n for n in range(1,4)] * 10
+
+d = {1: 'day1', 2: 'day2', 3: 'day3'}
+pathname = [d[n] for n in pathnos]
+
+f1 = [random.choice(['', 'Y']) for i in range(10)]
+f1 = [[x]*3 for x in f1]
+f1 = [i for s in f1 for i in s]
+
+f2 = [random.randint(0, 100) for i in range(30)]
+
+treatments = ['T1', 'T2', 'NoTrt']
+start = [random.choice(treatments) for i in range(30)] 
+end = start[1:]
+end.append(random.choice(treatments))
+end[2::3] = [random.choice(treatments) for i in range(10)] 
+
+data = pd.DataFrame({'USUBJID':ids, 'NODE_S':start, 'NODE_E':end, 'PATHNO':pathnos, 'PATHNAME':pathname, 'FILTER_Y':f1, 'FILTER_2':f2})
+data.to_csv('sankey_dummy_data.csv',index=False)
