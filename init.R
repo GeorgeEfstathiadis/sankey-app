f25936a5a1b1d4d24c4b744a94d4d6342d608b32
G.efstathiadis <54844705+GeorgeEfstathiadis@users.noreply.github.com>
2021-03-17 09:26:59 +0000

    Update init.R

--- init.R
+++ init.R
@@ -2,10 +2,8 @@ my_packages <- c("shiny", "shinydashboard", "shinycssloaders", "shinyBS",
 	"shinyWidgets", "networkD3", "highcharter", "DT", "htmltools", "htmlwidgets",
 	"dplyr", "stringr", "readxl", "haven", "RColorBrewer")
 
-install_if_missing <- function(p){
-	if (p %in% rownames(installed.packages()) == FALSE){
-		install.packages(p)
-	}
+for (p in my_packages){
+  if (p %in% rownames(installed.packages()) == FALSE){
+    install.packages(p)
+  }
 }
-
-invisible(sapply(my_packages, install_if_missing))
